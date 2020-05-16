f4f961be2e02a89d1f37045e1dd897bc8a28cc22
Dmitry Barskov <dmitry.barskov64@gmail.com>
2020-05-16 11:38:34 +0300

    Simplify code in LogicalSpecification
    Signed-off-by: Dmitry Barskov <dmitry.barskov64@gmail.com>

--- src/main/java/com/github/scfj/jpava/specifications/TextSpecification.java
+++ src/main/java/com/github/scfj/jpava/specifications/TextSpecification.java
@@ -3,7 +3,6 @@ package com.github.scfj.jpava.specifications;
 import com.github.scfj.jpava.specifications.compose.ComposeStrategy;
 import com.github.scfj.jpava.specifications.compose.OrCompose;
 import com.github.scfj.jpava.specifications.predicates.IgnoreCasePredicateStrategy;
-import com.github.scfj.jpava.specifications.predicates.MatchCasePredicateStrategy;
 import com.github.scfj.jpava.specifications.predicates.PredicateStrategy;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -22,18 +21,25 @@ public class TextSpecification<T> extends LogicalSpecification<T> {
     private final ComposeStrategy composeStrategy;
     private final PredicateStrategy predicateStrategy;
 
-    public TextSpecification(String searchQuery,
-                             FieldNames fieldNames,
-                             ComposeStrategy composeStrategy,
-                             PredicateStrategy predicateStrategy) {
+    public TextSpecification(
+            String searchQuery,
+            FieldNames fieldNames,
+            ComposeStrategy composeStrategy,
+            PredicateStrategy predicateStrategy
+    ) {
         this.fieldNames = fieldNames;
         this.searchQuery = searchQuery;
         this.composeStrategy = composeStrategy;
         this.predicateStrategy = predicateStrategy;
     }
 
-    public TextSpecification(String searchQuery, FieldNames fieldNames, ComposeStrategy composeStrategy) {
-        this(searchQuery, fieldNames, composeStrategy, new IgnoreCasePredicateStrategy(searchQuery));
+    public TextSpecification(
+            String searchQuery,
+            FieldNames fieldNames,
+            ComposeStrategy composeStrategy
+    ) {
+        this(searchQuery, fieldNames, composeStrategy,
+                new IgnoreCasePredicateStrategy(searchQuery));
     }
 
     public TextSpecification(String searchQuery, FieldNames fieldNames) {
@@ -41,22 +47,22 @@ public class TextSpecification<T> extends LogicalSpecification<T> {
     }
 
     @Override
-    public final Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        return composeStrategy.composePredicates(builder, predicatesForColumns(root, builder));
+    public final Predicate toPredicate(
+            Root<T> root,
+            CriteriaQuery<?> query,
+            CriteriaBuilder builder
+    ) {
+        return composeStrategy.composePredicates(
+                builder, predicatesForColumns(root, builder)
+        );
     }
 
-    public TextSpecification<T> except(String... fields) {
-        return new TextSpecification<>(searchQuery, fieldNames.except(fields), composeStrategy, predicateStrategy);
-    }
-
-    public TextSpecification<T> matchCase() {
-        return new TextSpecification<>(searchQuery, fieldNames, composeStrategy,
-                                       new MatchCasePredicateStrategy(searchQuery));
-    }
-
-    private Predicate[] predicatesForColumns(Root<?> root, CriteriaBuilder builder) {
+    private Predicate[] predicatesForColumns(
+            Root<?> root, CriteriaBuilder builder
+    ) {
         return fieldNames.stream()
-                .map(field -> predicateStrategy.buildPredicate(field, root, builder))
-                .toArray(Predicate[]::new);
+                .map(field -> predicateStrategy.buildPredicate(
+                        field, root, builder
+                )).toArray(Predicate[]::new);
     }
 }
